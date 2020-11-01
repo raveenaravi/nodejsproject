51a75304208e60c88496658ecd4efcc0c23c0699
raveenarvi <raveenar2012@gmail.com>
2020-11-01 20:50:22 +0530

    first commit

---
+++ controllers/category.js
@@ -0,0 +1,69 @@
+const Category = require("../models/category");
+
+exports.getCategoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, cate) => {
+    if (err) {
+      return res.status(400).json({
+        error: "CATEGORY not found in DB"
+      });
+    }
+    req.category = cate;
+    next();
+  });
+};
+
+exports.createCategory = (req, res) => {
+  const category = new Category(req.body);
+  category.save((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Unable to save CATEGORY in DB"
+      });
+    }
+    res.json({ category });
+  });
+};
+
+exports.getCategory = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.getAllCategory = (req, res) => {
+  Category.find().exec((err, categories) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Unable to find CATEGORY"
+      });
+    }
+    res.json(categories);
+  });
+};
+
+exports.updateCategory = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+
+  category.save((err, updatedCategory) => {
+    if (err) {
+      return res.status(400).json({
+        error: " CATEGORY updation FAILED "
+      });
+    }
+    res.json(updatedCategory);
+  });
+};
+
+exports.removeCategory = (req, res) => {
+  const category = req.category;
+
+  category.remove((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: "CATEGORY deletion FAILED"
+      });
+    }
+    res.json({
+      message: "DELETION SUCCESSFUL"
+    });
+  });
+};
